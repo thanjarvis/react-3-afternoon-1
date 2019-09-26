52bba7ff6f0d0da47a7f59fac15e5a9ef8017719
Than <than.jarvis@gmail.com>
2019-09-26 15:38:16 -0600

    project finished except blackdiamon

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -50,10 +50,14 @@ export default class Post extends Component {
   }
 
   render() {
+  
+    
     // This is destructuring! You can also think of it as being written as so:
       // const editing = this.state.editing
       // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
+    const{ text, date} = this.props;
+    
 
     return (
       // Main body of post
@@ -66,10 +70,9 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+            <span onClick={ () => this.props.deletePostFn(this.props.id) }>Delete</span>
           </div>
         </div>
-
         {/* This is where all the meta data of the post will go (who, when, where) */}
         <div className="Post__meta-data">
           <div className="Post__profile-picture">
@@ -79,7 +82,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">- {date}</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -95,10 +98,12 @@ export default class Post extends Component {
             // This has been pulled off of this.state via destructuring
             editing
             ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
+              <Edit text={text}
+                    id={this.props.id}
+                    hideEdit={ this.hideEdit }
+                    updatePostFn={this.props.updatePost} />
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+              <span className="Post__text">{text}</span>
           }
         </div>
 
